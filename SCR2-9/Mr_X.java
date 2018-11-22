7ab21fe356d7b06267633a7bc6c2086f8812a19d
mozKn1911 <14124@hakodate.kosen-ac.jp>
2018-11-22 10:51:23 +0900

    Mr_X追加

---
+++ SCR2-9/Mr_X.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Mr_X here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mr_X extends Actor
+{
+    /**
+     * Act - do whatever the Mr_X wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
