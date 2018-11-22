e820e1e4d2209a3f1aff57998fb02203f520e772
stmg335 <14082@hakodate.kosen-ac.jp>
2018-11-22 11:29:31 +0900

    sasamori

--- SCR2-9/hahaha.java
+++ SCR2-9/hahaha.java
@@ -14,6 +14,12 @@ public class hahaha extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
-    }    
+        if( Greenfoot.isKeyDown( "up" ) ){
+        setRotation(270);
+        move(1);
 }
+
+}
+
+    }    
+
